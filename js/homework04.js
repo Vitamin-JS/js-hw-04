28c573310cfe9ee1a7377ab890c48695823544ab
Vitamin-JS <Vpopchenkov@gmail.com>
2020-08-16 22:21:24 +0300

    Add правильное решение Задачи 4-1

--- js/homework04.js
+++ js/homework04.js
@@ -21,27 +21,32 @@
 // -------------------------------  Решение  ---------------------------------------------------------------
 //
 
-// Write code under this line
-const addIndex = (element, index) => element + index;
-// Write code under this line
-const subIndex = (element, index) => element - index;
+// // Write code under this line
+// const addIndex = (element, index) => element + index;
+// // Write code under this line
+// const subIndex = (element, index) => element - index;
 
-function mapArray(array, cb) {
-  "use strict";
-  // Write code under this line
-  const numbers = new Array();
-  for (let i = 0; i < array.length; i += 1) {
-    const element = array[i];
-    const index = i;
-    numbers[i] = cb(element, index);
-  }
-  return numbers;
-}
+// function mapArray(array, cb) {
+//   "use strict";
+//   // Write code under this line
+//   const numbers = new Array(array.length); // new Array(array.length) - правильное решение по автопроверке, но не понятно зачем?????????
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (numbers.length <= array.length) {
+//       const element = array[i];
+//       const index = i;
+//       numbers[i] = cb(element, index);
+//     }
+//   }
+//   return numbers;
+// }
 
-const arr = [1, 2, 3, 4, 5];
+// -----------------------------------------------------------------------------------------------------------
+//
 
-console.log(mapArray(arr, addIndex));
-// [1, 3, 5, 7, 9]
+// const arr = [1, 2, 3, 4, 5];
 
-//console.log(mapArray(arr, subIndex));
-// [1, 1, 1, 1, 1]
+// // console.log(mapArray(arr, addIndex));
+// // [1, 3, 5, 7, 9]
+
+// console.log(mapArray(arr, subIndex));
+// // [1, 1, 1, 1, 1]
